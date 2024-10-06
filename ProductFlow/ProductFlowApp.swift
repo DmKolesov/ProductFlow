8ecc3df475223b7262670deac1da2eaa4a5e97ad
Dima Kolesov <kolesov.public@gmail.com>
2024-10-07 00:33:13 +0300

    Initial

---
+++ ProductFlow/ProductFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductFlowApp.swift
+//  ProductFlow
+//
+//  Created by Dima Kolesov on 06.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProductFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
