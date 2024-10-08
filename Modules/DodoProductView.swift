9084cef7049fe832feab4b7146802cf4165722d5
Dima Kolesov <kolesov.public@gmail.com>
2024-10-08 22:55:18 +0300

    Add UI CustomSegmentPicker

---
+++ Modules/DodoProductView.swift
@@ -0,0 +1,28 @@
+//
+//  DodoProductView.swift
+//  ProductFlow
+//
+//  Created by Dima Kolesov on 07.10.2024.
+//
+
+import SwiftUI
+
+struct DodoProductView: View {
+    
+    
+    var body: some View {
+        
+        //NavigationStack
+        VStack {
+            
+            
+        }
+        
+    }
+}
+
+
+
+#Preview {
+    DodoProductView()
+}
