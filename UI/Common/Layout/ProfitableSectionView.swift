e70bcd6b39c858185573f218a060b2cbbea3b081
Dima Kolesov <kolesov.public@gmail.com>
2024-10-08 23:12:17 +0300

    Add ProfitableSectionView

---
+++ UI/Common/Layout/ProfitableSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfitableSectionView.swift
+//  ProductFlow
+//
+//  Created by Dima Kolesov on 08.10.2024.
+//
+
+import SwiftUI
+
+struct ProfitableSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfitableSectionView()
+}
